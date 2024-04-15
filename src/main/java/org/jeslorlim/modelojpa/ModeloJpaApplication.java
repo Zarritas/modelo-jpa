ba6415a72478f226ab15755ef52e9ef86d38cf2d
Zarritas <jlorenzolimon@gmail.com>
2024-04-15 09:54:16 +0200

    Creacion Proyecto

---
+++ src/main/java/org/jeslorlim/modelojpa/ModeloJpaApplication.java
@@ -0,0 +1,13 @@
+package org.jeslorlim.modelojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModeloJpaApplication.class, args);
+    }
+
+}
